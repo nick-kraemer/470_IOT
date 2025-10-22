96c5e4db644c6b8daa907ebfbc2a50b3e3fd1d32
nick-kraemer <156550431+nick-kraemer@users.noreply.github.com>
2025-10-21 20:32:20 -0700

    Create config.cpp

---
+++ Projects/Sensor_database_ESP/config.cpp
@@ -0,0 +1,7 @@
+// config.cpp
+#include "config.h"
+
+// Put your real credentials here
+const char* WIFI_SSID     = "wifiname";
+const char* WIFI_PASSWORD = "wifipassword";
+const char* BASE_URL      = "https://nickkaemer.com/Sensor_data2.php";
